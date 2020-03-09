dca4c56c00397edf555e2c4cc7fa784f438b4330
kevin.li <kevin.li@duethealth.com>
2020-03-09 16:49:39 -0400

    Fixed with shim

---
+++ Foo/RxSwift/RxSwift.h
@@ -0,0 +1,19 @@
+//
+//  RxSwift.h
+//  RxSwift
+//
+//  Created by Kevin Li on 3/9/20.
+//  Copyright © 2020 kevinli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxSwift.
+FOUNDATION_EXPORT double RxSwiftVersionNumber;
+
+//! Project version string for RxSwift.
+FOUNDATION_EXPORT const unsigned char RxSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwift/PublicHeader.h>
+
+
